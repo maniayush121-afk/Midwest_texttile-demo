2c991e1752a8d7ad2149acbc60b2c7f77262d6c8
Mohit Gahlyan <dev@starkedge.com>
2025-12-02 16:47:28 +0530

    project update

---
+++ js/js.js
@@ -0,0 +1,47 @@
+console.log("Script loaded successfully");
+
+// document.getElementsByClassName("btn")[0].style.backgroundColor = "red";
+
+// console.log(document.getElementsByClassName("btn")[0]);
+
+console.log("welcome to javascript programming");
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+console.log(a + b + c + d + e);
+var f = 1
+var g = 2
+var h = 3
+var i = 4
+var j = 5
+console.log(f==b);
+console.log(f&b);
+console.log(h|i);
+console.log(h&&i);
+
+alert("This is an alert box!");
+
+// document.getElementsByClassName("h1")[0].style.border = '2px solid blue';
+
+// console.log(document.getElementsByClassName("h1")[0]);
+console.log("This is the end of the script.");
+
+function showText() {
+
+ let a =123;
+
+let text = "hey  there! This is JavaScript programming.";
+
+console.log(a == text);
+
+}
+showText();
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $(".navbar").slideToggle("slow");
+    });
+});
